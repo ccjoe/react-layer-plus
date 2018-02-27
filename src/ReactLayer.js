a55c4ee7a096c85422727e0dbd1c19dee9946b71
JOE <icareu.joe@gmail.com>
2018-02-27 15:25:01 +0800

    Revert "Revert "fix外部传参zIndex,position无效的bug""

--- src/ReactLayer.js
+++ src/ReactLayer.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 
-var mixStyle = function (offset) {
-    return Object.assign({ position: 'absolute', zIndex: 100 }, offset)
-}
-
 // Fix for IE8-'s Element.getBoundingClientRect()
 if ('TextRectangle' in window && !('width' in TextRectangle.prototype)) {
     Object.defineProperties(TextRectangle.prototype, {
@@ -87,7 +83,7 @@ class ReactLayer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            offset: mixStyle(props.offset) || {},		//position
+            offset: Object.assign({ zIndex: 100 ,position:'absolute'},props.offset),		//position
             show: props.show || false
         }
         // this.showLayer = throttle(this.showLayer, 500, true)
@@ -165,8 +161,7 @@ class ReactLayer extends Component {
 
     show(ok) {
         if (ok) {
-            this.setState({ show: ok, offset: mixStyle(this.setDefaultPos()) })
-            this.setState({ offset: mixStyle(this.setAddtionPos()) })
+            this.setState({show: ok, offset: Object.assign({},this.state.offset,this.setDefaultPos(),this.setAddtionPos())})
         } else {
             this.setState({ show: false })
         }
